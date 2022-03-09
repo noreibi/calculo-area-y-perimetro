529d1c9b19fcfb2aa9aa52bbc6140cabc8b013fc
Noreibi Leal <noreibileal@gmail.com>
2022-03-09 12:21:51 -0300

    agregando validaciones

--- isosceles.js
+++ isosceles.js
@@ -12,13 +12,20 @@ function alturaTriangulo(ladoA, ladoBase) {
 }
 
 function calcularPerimetroIsosceles() {
+  //recuperar los datos: lo que quiere comer
   const inputladoA = document.getElementById("ladoATrianguloPerimetro");
   const inputBase = document.getElementById("baseTrianguloPerimetro");
-
   const ladoA = parseInt(inputladoA.value);
   const ladoBase = parseInt(inputBase.value);
-
+  //validar los datos:
+  const resultadoValidacion = validarDatos(ladoA, ladoBase);
+  if (resultadoValidacion.error) {
+    alert(resultadoValidacion.mensaje);
+    return;
+  }
+  //calcular o solicitarselos al modelo
   const perimetro = perimetroTriangulo(ladoA, ladoBase);
+  //imprimir
   alert(perimetro);
 }
 
@@ -40,3 +47,27 @@ function calcularAlturaIsosceles() {
   const altura = alturaTriangulo(inputladoA.value, inputladoBase.value);
   alert(altura);
 }
+
+function validarDatos(ladoA, base) {
+  if (!ladoA) return { error: true, mensaje: "El lado A no esta definido" };
+  if (!base) return { error: true, mensaje: "Necesitas escribir la base" };
+  if (ladoA < 1)
+    return {
+      error: true,
+      mensaje: "Los lados del triangulo deben ser mayores a cero",
+    };
+  if (base < 1)
+    return {
+      error: true,
+      mensaje: "La base debe ser mayor a cero",
+    };
+  if (!parseInt(ladoA) || !parseInt(base))
+    return { error: true, mensaje: "por favor introduzca un numero valido" };
+
+  return { error: false, mensaje: "no hubo errores" };
+}
+
+console.log(typeof 4);
+console.log(typeof "hola");
+console.log(typeof { nombre: "Ali" });
+console.log(typeof []);
